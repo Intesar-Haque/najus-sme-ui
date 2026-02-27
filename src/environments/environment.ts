8f8a0b739785a55f63adb8f137000a33ef4f0710
Intesar <intesarhaque99@gmail.com>
2026-02-27 15:16:41 +0600

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // api: 'http://127.0.0.1:8000/api',
-  api: 'http://najus-sme.ntilearnbd.com/api',
+  api: 'https://najus-sme.ntilearnbd.com/api',
 };
